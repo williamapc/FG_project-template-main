4f3101db2b71b7c58813581b327c7e87e1a45814
williamapc <w.h.b.a.p.c@gmail.com>
2026-02-15 16:44:37 +0100

    modif config.py

--- streamlit_app/config.py
+++ streamlit_app/config.py
@@ -11,11 +11,16 @@ TITLE = "My Awesome App"
 
 TEAM_MEMBERS = [
     Member(
-        name="John Doe",
-        linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/",
-        github_url="https://github.com/charlessutton",
+        name="Fayçal D",
+        #linkedin_url="https://www.linkedin.com/in/charlessuttonprofile/",
+        github_url="https://github.com/faycal77"
     ),
     Member("Jane Doe"),
+    Member(
+        name="Guillaume A",
+        github_url="https://github.com/williamapc"
+    ),
+    Member("Jane Doe")
 ]
 
 PROMOTION = "Promotion Bootcamp Data Scientist - April 2021"
